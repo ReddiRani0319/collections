e97f6dd3ab4b97e97cdce79a393f8d124b7ca683
ReddiRani0319 <116836875+ReddiRani0319@users.noreply.github.com>
2022-10-28 00:12:37 +0530

    First commit

---
+++ src/hashmap/getValues.java
@@ -0,0 +1,17 @@
+package hashmap;
+
+import java.util.HashMap;
+
+public class getValues {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap hm=new Hashmap();
+		hm.put(15,"S");
+		hm.put(20,"Hello");
+		hm.put(16, "D");
+		hm.put(18,"R");
+		System.out.println(hms);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author medrani
+ *
+ */
+module collections {
+}
